7132cae6c628344d65b254053a6b0b547d8df0e3
Dan Kingston <d@nkingston.com>
2019-12-30 10:15:07 +0000

    Template filter for highlighting search terms

---
+++ barriers/templatetags/highlight.py
@@ -0,0 +1,19 @@
+import re
+
+from django import template
+from django.utils.safestring import mark_safe
+
+
+register = template.Library()
+
+
+@register.filter(needs_autoescape=True)
+def highlight(value, arg, autoescape=True):
+    result = re.sub(
+        fr"({arg})",
+        r"<span class='highlight'>\1</span>",
+        value,
+        flags=re.IGNORECASE
+    )
+
+    return mark_safe(result)
--- config/settings/base.py
+++ config/settings/base.py
@@ -44,7 +44,9 @@ THIRD_PARTY_APPS = [
     'sass_processor',
 ]
 
-LOCAL_APPS = []
+LOCAL_APPS = [
+    'barriers',
+]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
