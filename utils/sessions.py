ccb53cdec1cb5ec26a6ce36a4b9aa759123148bb
Feri Csepregi <feri@31.digital>
2020-01-07 17:28:44 +0000

    fixup! MAR-146 - update to how sessions are initialised during login - update to sso client to use the token from env var - update market access api client to handle JSONDecodeError-s - add logout view that will flush the django session - update context_processor to pull user info from session

--- utils/sessions.py
+++ utils/sessions.py
@@ -1,5 +1,4 @@
 from users.helpers import sync_user
-# from utils.api_client import MarketAccessAPIClient
 
 
 def init_session(session, access_token):
@@ -15,26 +14,5 @@ def init_session(session, access_token):
         del session['oauth_state_id']
         session['sso_token'] = access_token
         user_synced = sync_user(session)
-        # client = MarketAccessAPIClient(access_token)
-        #
-        # # sync user
-        # # Ref of user_data as of 2020/01/06
-        # #   id - INT
-        # #   username - STR
-        # #   last_login - STR
-        # #   first_name - STR
-        # #   last_name - STR
-        # #   email - STR
-        # #   location - STR
-        # #   internal - BOOL
-        # #   user_profile - DICT
-        # #       watchList - DICT
-        # #   permitted_applications - LIST
-        #
-        # # user_data = session['api_client'].get('whoami') or {}
-        # user_data = client.get('whoami') or {}
-        # if user_data:
-        #     init_successful = True
-        # session['user_data'] = user_data
 
     return user_synced
