d69d3f544fe70a39cce1eb684753f2640dc3ed7c
Feri Csepregi <feri@31.digital>
2020-01-08 17:45:29 +0000

    MAR-151 - move logic from template to context processor to determine which apps to display a link for in the header

--- utils/context_processors.py
+++ utils/context_processors.py
@@ -1,65 +1,92 @@
-def permitted_applications(request):
-    datahubDomain = "https://www.datahub.trade.gov.uk/"
-    miDomain = 'https://mi.exportwins.service.trade.gov.uk/'
-    findExportersDomain = 'https://find-exporters.datahub.trade.gov.uk/'
-    marketAccessDomain = 'https://market-access.trade.gov.uk/'
-    return {
-        'user': request.session.get("user_data") or None,
-        'apps': [
-            {
-                'permittedKey': 'datahub-crm',
-                'activeKey': 'datahub-companies',
-                'canShow': True,
-                'name': 'Companies',
-                'href': (datahubDomain + 'companies')
-            }, {
-                'permittedKey': 'datahub-crm',
-                'activeKey': 'datahub-contacts',
-                'canShow': True,
-                'name': 'Contacts',
-                'href': (datahubDomain + 'contacts')
-            }, {
-                'permittedKey': 'datahub-crm',
-                'activeKey': 'datahub-events',
-                'canShow': True,
-                'name': 'Events',
-                'href': (datahubDomain + 'events')
-            }, {
-                'permittedKey': 'datahub-crm',
-                'activeKey': 'datahub-interactions',
-                'canShow': True,
-                'name': 'Interactions',
-                'href': (datahubDomain + 'interactions')
-            }, {
-                'permittedKey': 'datahub-crm',
-                'activeKey': 'datahub-investments',
-                'canShow': True,
-                'name': 'Investments',
-                'href': (datahubDomain + 'investments')
-            }, {
-                'permittedKey': 'datahub-crm',
-                'activeKey': 'datahub-orders',
-                'canShow': True,
-                'name': 'Orders',
-                'href': (datahubDomain + 'omis')
-            }, {
-                'permittedKey': 'datahub-mi',
-                'activeKey': 'datahub-mi',
-                'canShow': True,
-                'name': 'Dashboards',
-                'href': miDomain
-            }, {
-                'permittedKey': 'find-exporters',
-                'activeKey': 'find-exporters',
-                'canShow': True,
-                'name': 'Find exporters',
-                'href': findExportersDomain
-            }, {
-                'permittedKey': 'market-access',
-                'activeKey': 'market-access',
-                'canShow': True,
-                'name': 'Market Access',
-                'href': marketAccessDomain
-            }
-        ]
+class Domains:
+    DATA_HUB = "https://www.datahub.trade.gov.uk/"
+    MI = "https://mi.exportwins.service.trade.gov.uk/"
+    FIND_EXPORTERS = "https://find-exporters.datahub.trade.gov.uk/"
+    MARKET_ACCESS = "https://market-access.trade.gov.uk/"
+
+
+def apps(user):
+    apps = [
+        {
+            'name': 'Companies',
+            'permittedKey': 'datahub-crm',
+            'activeKey': 'datahub-companies',
+            'canShow': True,
+            'href': (Domains.DATA_HUB + 'companies')
+        },
+        {
+            'name': 'Contacts',
+            'permittedKey': 'datahub-crm',
+            'activeKey': 'datahub-contacts',
+            'canShow': True,
+            'href': (Domains.DATA_HUB + 'contacts')
+        },
+        {
+            'name': 'Events',
+            'permittedKey': 'datahub-crm',
+            'activeKey': 'datahub-events',
+            'canShow': True,
+            'href': (Domains.DATA_HUB + 'events')
+        },
+        {
+            'name': 'Interactions',
+            'permittedKey': 'datahub-crm',
+            'activeKey': 'datahub-interactions',
+            'canShow': True,
+            'href': (Domains.DATA_HUB + 'interactions')
+        },
+        {
+            'name': 'Investments',
+            'permittedKey': 'datahub-crm',
+            'activeKey': 'datahub-investments',
+            'canShow': True,
+            'href': (Domains.DATA_HUB + 'investments')
+        },
+        {
+            'name': 'Orders',
+            'permittedKey': 'datahub-crm',
+            'activeKey': 'datahub-orders',
+            'canShow': True,
+            'href': (Domains.DATA_HUB + 'omis')
+        },
+        {
+            'name': 'Dashboards',
+            'permittedKey': 'datahub-mi',
+            'activeKey': 'datahub-mi',
+            'canShow': True,
+            'href': Domains.MI
+        },
+        {
+            'name': 'Find exporters',
+            'permittedKey': 'find-exporters',
+            'activeKey': 'find-exporters',
+            'canShow': True,
+            'href': Domains.FIND_EXPORTERS
+        },
+        {
+            'name': 'Market Access',
+            'permittedKey': 'market-access',
+            'activeKey': 'market-access',
+            'canShow': True,
+            'href': Domains.MARKET_ACCESS
+        },
+    ]
+
+    # Find out which of the apps the user have access to
+    permitted_applications = user.get("permitted_applications", [])
+    permitted_keys = [app["key"] for app in permitted_applications]
+
+    visible_apps = [app for app in apps if app["permittedKey"] in permitted_keys and app["canShow"]]
+
+    return visible_apps
+
+
+def user_scope(request):
+    user = request.session.get("user_data", {})
+
+    extra_context = {
+        'user': user,
+        'apps': apps(user),
     }
+
+    return extra_context
