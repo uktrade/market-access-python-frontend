e0bd6d4589e111e25675381a33ca66e404cc0c75
Feri <feri@31.digital>
2020-01-09 17:13:07 +0000

    MAR-151 - move logic from template to context processor to determine which apps to display a link for in the header (#5)

--- config/settings/base.py
+++ config/settings/base.py
@@ -83,7 +83,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'utils.context_processors.permitted_applications',
+                'utils.context_processors.user_scope',
             ],
         },
     },
