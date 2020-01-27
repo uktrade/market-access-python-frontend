a5ec3400169e2234e0202a03067c4aed11f8b3b1
Dan Kingston <d@nkingston.com>
2020-01-27 17:10:42 +0000

    MAR-162: Test adding a note with attachments

--- core/tests.py
+++ core/tests.py
@@ -14,7 +14,9 @@ class MarketAccessTestCase(TestCase):
     def setUp(self):
         self.init_session()
         self.init_metadata_patcher()
-        self.init_get_barrier()
+        self.init_get_barrier_patcher()
+        self.init_get_history_patcher()
+        self.init_get_interactions_patcher()
 
     def init_session(self):
         session = self.client.session
@@ -30,11 +32,29 @@ class MarketAccessTestCase(TestCase):
         self.mock_metadata_get.return_value = self.metadata_json
         self.addCleanup(self.metadata_patcher.stop)
 
-    def init_get_barrier(self):
+    def init_get_barrier_patcher(self):
         self.get_barrier_patcher = patch("utils.api_client.BarriersResource.get")
         self.mock_get_barrier = self.get_barrier_patcher.start()
         self.mock_get_barrier.return_value = BarriersResource.model(self.barriers[0])
         self.addCleanup(self.get_barrier_patcher.stop)
+
+    def init_get_barrier_patcher(self):
+        self.get_barrier_patcher = patch("utils.api_client.BarriersResource.get")
+        self.mock_get_barrier = self.get_barrier_patcher.start()
+        self.mock_get_barrier.return_value = BarriersResource.model(self.barriers[0])
+        self.addCleanup(self.get_barrier_patcher.stop)
+
+    def init_get_history_patcher(self):
+        self.get_history_patcher = patch("utils.api_client.BarriersResource.get_history")
+        self.mock_get_history = self.get_history_patcher.start()
+        self.mock_get_history.return_value = []
+        self.addCleanup(self.get_history_patcher.stop)
+
+    def init_get_interactions_patcher(self):
+        self.get_interactions_patcher = patch("utils.api_client.InteractionsResource.list")
+        self.mock_get_interactions = self.get_interactions_patcher.start()
+        self.mock_get_interactions.return_value = []
+        self.addCleanup(self.get_interactions_patcher.stop)
 
     def update_session(self, data):
         session = self.client.session
