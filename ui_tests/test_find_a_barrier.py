965e709042e3e307db9001d45444cb4b22b36f73
Dan Kingston <d@nkingston.com>
2020-02-07 17:27:46 +0000

    MAR-160: Find a barrier end to end tests

--- ui_tests/test_find_a_barrier.py
+++ ui_tests/test_find_a_barrier.py
@@ -35,3 +35,51 @@ def test_find_by_code(browser):
     assert browser.is_element_present_by_css(
         f'[data-barrier-id="{barrier_id}"]'
     )
+
+
+def test_priority_search_filter(browser):
+    """
+    Only relevant barriers should show when searching by priority
+    """
+    barrier_id = settings.TEST_BARRIER_ID
+
+    browser.visit(f"{settings.BASE_URL}find-a-barrier")
+
+    checkbox = browser.find_by_css("input[name=priority][value='HIGH']")
+    checkbox.find_by_xpath('./following-sibling::label').click()
+
+    checkbox = browser.find_by_css("input[name=priority][value='LOW']")
+    checkbox.find_by_xpath('./following-sibling::label').click()
+
+    browser.find_by_css(
+        'input[type=submit][value="Apply filters"]'
+    ).first.click()
+
+    for result in browser.find_by_css('.filter-results-list__item'):
+        priority = result.find_by_css('.priority-marker-wrapper').value
+        assert priority in ("High priority", "Low priority")
+
+
+def test_status_search_filter(browser):
+    """
+    Only relevant barriers should show when searching by status
+    """
+    barrier_id = settings.TEST_BARRIER_ID
+
+    browser.visit(f"{settings.BASE_URL}find-a-barrier")
+
+    # Open: In progress
+    checkbox = browser.find_by_css("input[name=status][value='2']")
+    checkbox.find_by_xpath('./following-sibling::label').click()
+
+    # Resolved: In full
+    checkbox = browser.find_by_css("input[name=status][value='4']")
+    checkbox.find_by_xpath('./following-sibling::label').click()
+
+    browser.find_by_css(
+        'input[type=submit][value="Apply filters"]'
+    ).first.click()
+
+    for result in browser.find_by_css('.filter-results-list__item'):
+        status = result.find_by_css('.barrier-status-badge').value
+        assert status in ("Open: In progress", "Resolved: In full")
