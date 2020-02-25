6fae5f097b73c56e0f37f191ca93d725270e4d5f
Dan Kingston <d@nkingston.com>
2020-02-25 15:25:16 +0000

    PEP8

--- config/settings/base.py
+++ config/settings/base.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import json
 import os
 
 from django.utils.log import DEFAULT_LOGGING
