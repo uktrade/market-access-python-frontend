01acfd2327a9159ef19943ecda3a5213ffd086bf
Feri Csepregi <feri@31.digital>
2020-03-20 23:18:11 +0000

    fixup! MAR-54 - highlight unseen event list items on barrier details page

---
+++ tests/barriers/test_barrier_details_view.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timezone
+from http import HTTPStatus
+
+from django.urls import reverse
+from mock import patch
+
+from barriers.models import HistoryItem
+from core.tests import MarketAccessTestCase
+
+
+class BarrierViewTestCase(MarketAccessTestCase):
+
+    @patch('utils.api.resources.BarriersResource.get_history')
+    def test_barrier_view_has_highlighted_event_list_items(self, mock_history):
+        mock_history.return_value = [HistoryItem(result) for result in self.history]
+        css_class = 'unseen__event-list__item'
+        expected_css_class_count = 2
+        self.barrier["last_seen_on"] = str(datetime(2020, 3, 19, 12, 30, tzinfo=timezone.utc))
+
+        response = self.client.get(
+            reverse("barriers:barrier_detail", kwargs={"barrier_id": self.barrier["id"]})
+        )
+        html = response.content.decode('utf8')
+
+        assert HTTPStatus.OK == response.status_code
+        unseen_events_count = html.count(css_class)
+        assert expected_css_class_count == unseen_events_count, \
+            f'Expected {expected_css_class_count} unseen events, got: {unseen_events_count}'
+
+    @patch('utils.api.resources.BarriersResource.get_history')
+    def test_barrier_view_without_highlighted_event_list_items(self, mock_history):
+        mock_history.return_value = [HistoryItem(result) for result in self.history]
+        css_class = 'unseen__event-list__item'
+        expected_css_class_count = 0
+        self.barrier["last_seen_on"] = str(datetime(2020, 4, 19, 12, 30, tzinfo=timezone.utc))
+
+        response = self.client.get(
+            reverse("barriers:barrier_detail", kwargs={"barrier_id": self.barrier["id"]})
+        )
+        html = response.content.decode('utf8')
+
+        assert HTTPStatus.OK == response.status_code
+        unseen_events_count = html.count(css_class)
+        assert expected_css_class_count == unseen_events_count, \
+            f'Expected {expected_css_class_count} unseen events, got: {unseen_events_count}'
