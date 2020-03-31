5d65160884cf78bc411063efd41efab5bfa5efc5
dankingston <d@nkingston.com>
2020-03-31 10:57:27 +0100

    MAR-268: Ensure ordering parameter is set when fetching csv (#37)
    * MAR-268: Ensure ordering parameter is set when fetching csv
    
    * MAR-268: Update download csv test

--- tests/barriers/test_download_barriers.py
+++ tests/barriers/test_download_barriers.py
@@ -51,4 +51,5 @@ class DownloadBarriersTestCase(MarketAccessTestCase):
             status="1,2,7",
             user="1",
             archived="0",
+            ordering="-reported_on",
         )
