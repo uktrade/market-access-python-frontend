9849ebb76e353591f15fa40478c89a7e158e8a58
Dan Kingston <d@nkingston.com>
2020-04-30 10:46:40 +0100

    Send WTO search parameters as list instead of individual fields

--- barriers/forms/search.py
+++ barriers/forms/search.py
@@ -19,26 +19,21 @@ class BarrierSearchForm(forms.Form):
     user = forms.BooleanField(label="My barriers", required=False,)
     team = forms.BooleanField(label="My team barriers", required=False,)
     only_archived = forms.BooleanField(label="Only archived barriers", required=False,)
-    wto_has_been_notified = forms.BooleanField(label="Notified", required=False,)
-    wto_should_be_notified = forms.BooleanField(label="Not notified but should be", required=False,)
-    has_wto_raised_date = forms.BooleanField(label="Raised", required=False,)
-    has_wto_committee_raised_in = forms.BooleanField(label="Discussed bilaterally", required=False,)
-    has_wto_case_number = forms.BooleanField(label="Subject to a dispute settlement", required=False,)
-    has_no_wto_information = forms.BooleanField(label="No WTO information provided", required=False,)
+    wto = forms.MultipleChoiceField(
+        label="WTO",
+        choices=(
+            ("wto_has_been_notified", "Notified"),
+            ("wto_should_be_notified", "Not notified but should be"),
+            ("has_wto_committee_raised_in", "Raised"),
+            ("has_wto_raised_date", "Discussed bilaterally"),
+            ("has_wto_case_number", "Subject to a dispute settlement"),
+            ("has_no_wto_information", "No WTO information provided"),
+        ),
+        required=False,
+    )
 
     filter_groups = {
         "show": {"label": "Show", "fields": ("user", "team", "only_archived")},
-        "wto": {
-            "label": "WTO",
-            "fields": (
-                "wto_has_been_notified",
-                "wto_should_be_notified",
-                "has_wto_raised_date",
-                "has_wto_committee_raised_in",
-                "has_wto_case_number",
-                "has_no_wto_information",
-            )
-        }
     }
 
     def __init__(self, metadata, *args, **kwargs):
@@ -76,12 +71,7 @@ class BarrierSearchForm(forms.Form):
             "user": data.get("user"),
             "team": data.get("team"),
             "only_archived": data.get("only_archived"),
-            "wto_has_been_notified": data.get("wto_has_been_notified"),
-            "wto_should_be_notified": data.get("wto_should_be_notified"),
-            "has_wto_raised_date": data.get("has_wto_raised_date"),
-            "has_wto_committee_raised_in": data.get("has_wto_committee_raised_in"),
-            "has_wto_case_number": data.get("has_wto_case_number"),
-            "has_no_wto_information": data.get("has_no_wto_information"),
+            "wto": data.getlist("wto"),
         }
         return {k: v for k, v in cleaned_data.items() if v}
 
@@ -226,20 +216,8 @@ class BarrierSearchForm(forms.Form):
         params["tags"] = ",".join(self.cleaned_data.get("tags", []))
         params["team"] = self.cleaned_data.get("team")
         params["user"] = self.cleaned_data.get("user")
+        params["wto"] = self.cleaned_data.get("wto")
         params["archived"] = self.cleaned_data.get("only_archived") or "0"
-
-        params["wto_has_been_notified"] = self.cleaned_data.get("wto_has_been_notified")
-        params["wto_should_be_notified"] = self.cleaned_data.get(
-            "wto_should_be_notified"
-        )
-        params["has_wto_raised_date"] = self.cleaned_data.get("has_wto_raised_date")
-        params["has_wto_committee_raised_in"] = self.cleaned_data.get(
-            "has_wto_committee_raised_in"
-        )
-        params["has_wto_case_number"] = self.cleaned_data.get("has_wto_case_number")
-
-        if self.cleaned_data.get("has_no_wto_information"):
-            params["has_wto_profile"] = "0"
 
         return {k: v for k, v in params.items() if v}
 
