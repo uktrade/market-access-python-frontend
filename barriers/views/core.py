b04db9c1b601ce37cedf2ce4d4d43a6a671f239f
Dan Kingston <d@nkingston.com>
2020-05-06 16:33:24 +0100

    Use my barriers and team barriers saved searches

--- barriers/views/core.py
+++ barriers/views/core.py
@@ -17,11 +17,15 @@ class Dashboard(TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super().get_context_data(**kwargs)
         client = MarketAccessAPIClient(self.request.session.get("sso_token"))
+        my_barriers_saved_search = client.saved_searches.get("my-barriers")
+        team_barriers_saved_search = client.saved_searches.get("team-barriers")
         saved_searches = client.saved_searches.list()
 
         context_data.update(
             {
                 "page": "dashboard",
+                "my_barriers_saved_search": my_barriers_saved_search,
+                "team_barriers_saved_search": team_barriers_saved_search,
                 "saved_searches": saved_searches,
             }
         )
@@ -56,14 +60,14 @@ class FindABarrier(PaginationMixin, SearchFormMixin, FormView):
 
         client = MarketAccessAPIClient(self.request.session.get("sso_token"))
 
-        if form.cleaned_data.get("search_id") is not None:
-            saved_search_id = form.cleaned_data.get("search_id")
-            saved_search = client.saved_searches.get(saved_search_id)
+        saved_search = self.get_saved_search(form)
+        if saved_search:
             context_data["saved_search"] = saved_search
             form_filters = form.get_raw_filters()
             context_data["have_filters_changed"] = nested_sort(
                 form_filters
             ) != nested_sort(saved_search.filters)
+            context_data["search_title"] = saved_search.name
 
         barriers = client.barriers.list(
             ordering="-reported_on",
@@ -82,18 +86,24 @@ class FindABarrier(PaginationMixin, SearchFormMixin, FormView):
                 ),
                 "pageless_querystring": self.get_pageless_querystring(),
                 "page": "find-a-barrier",
-                "search_title": self.get_search_title(form),
             }
         )
 
         return context_data
 
-    def get_search_title(self, form):
+    def get_saved_search(self, form):
+        client = MarketAccessAPIClient(self.request.session.get("sso_token"))
+
+        if form.cleaned_data.get("search_id") is not None:
+            saved_search_id = form.cleaned_data.get("search_id")
+            return client.saved_searches.get(saved_search_id)
+
         filters = form.get_raw_filters()
+
         if filters == {"user": "1"}:
-            return "My barriers"
+            return client.saved_searches.get("my-barriers")
         elif filters == {"team": "1"}:
-            return "Team barriers"
+            return client.saved_searches.get("team-barriers")
 
     def get_pageless_querystring(self):
         params = self.request.GET.copy()
