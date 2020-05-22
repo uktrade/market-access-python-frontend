797d1784c83bff33151264456c6653760a89e349
Feri <feri@31.digital>
2020-05-22 15:08:52 +0100

    MAR-63 - remove add team member view (#60)

--- barriers/forms/teams.py
+++ barriers/forms/teams.py
@@ -7,10 +7,3 @@ class UserSearchForm(forms.Form):
         max_length=255,
         required=False,
     )
-
-
-class AddTeamMemberForm(forms.Form):
-    user = forms.CharField(label="Name", max_length=255, widget=forms.HiddenInput(),)
-    role = forms.CharField(
-        label="Role", max_length=255, error_messages={"required": "Enter a role"},
-    )
