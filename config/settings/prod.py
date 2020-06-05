d3c4e96b45ec0d7db36f06a202a76c04eca41857
Dan Kingston <d@nkingston.com>
2020-06-05 10:20:01 +0100

    Only use ECSFormatter on prod

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,32 @@
+import sys
 from .base import *                 # noqa
 
+from django_log_formatter_ecs import ECSFormatter
+
+
 DJANGO_ENV = 'prod'
+
 DEBUG = False
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["ecs"],
+            "level": DJANGO_LOG_LEVEL
+        }
+    },
+}
