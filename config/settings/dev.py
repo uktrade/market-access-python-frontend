b210e6a35e0a570bdeb650d348086bdb42dc9923
dankingston <d@nkingston.com>
2020-06-08 12:10:36 +0100

    MAR-247: Add correlation ids to logging (#78)
    * Use django_log_formatter_ecs for logging

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -1,3 +1,30 @@
+import sys
 from .base import *                 # noqa
 
+from django_log_formatter_ecs import ECSFormatter
+
 DJANGO_ENV = 'dev'
+
+# Unable to put this in base.py because of circular import problem with ECSFormatter
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["ecs"],
+            "level": DJANGO_LOG_LEVEL
+        }
+    },
+}
--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,4 +1,33 @@
+import sys
 from .base import *                 # noqa
 
+from django_log_formatter_ecs import ECSFormatter
+
+
 DJANGO_ENV = 'prod'
+
 DEBUG = False
+
+# Unable to put this in base.py because of circular import problem with ECSFormatter
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "ecs_formatter": {
+            "()": ECSFormatter,
+        },
+    },
+    "handlers": {
+        "ecs": {
+            "formatter": "ecs_formatter",
+            "class": "logging.StreamHandler",
+            "stream": sys.stdout,
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["ecs"],
+            "level": DJANGO_LOG_LEVEL
+        }
+    },
+}
