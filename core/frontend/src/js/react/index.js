aa0a383a6d5e97b096889cc9b08e638cdab330e9
Dan Kingston <d@nkingston.com>
2020-07-24 12:11:12 +0100

    Get CSRF token in React

--- core/frontend/src/js/react/index.js
+++ core/frontend/src/js/react/index.js
@@ -2,10 +2,20 @@ import React from "react";
 import ReactDOM from 'react-dom'
 
 import CommodityForm from "./commodities/CommodityForm"
+import {getCSRFToken} from "./utils"
 
 
-function renderCommodities(csrfToken, confirmedCommodities) {
-    ReactDOM.render(<CommodityForm csrfToken={csrfToken} confirmedCommodities={confirmedCommodities} />, document.getElementById('react-app'));
+function renderCommodities(confirmedCommodities, countries) {
+  const csrfToken = getCSRFToken()
+  ReactDOM.render(
+    <CommodityForm
+      csrfToken={csrfToken}
+      confirmedCommodities={confirmedCommodities}
+      countries={countries}
+    />,
+    document.getElementById('react-app')
+  );
 }
+
 
 export {renderCommodities}
