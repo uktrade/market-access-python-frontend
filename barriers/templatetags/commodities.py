d09c78d209a21f077cf250debf43701f660c19ee
Feri Csepregi <feri@31.digital>
2020-10-27 14:42:32 +0000

    MAR-662 - add labels for commodity code boxes

---
+++ barriers/templatetags/commodities.py
@@ -0,0 +1,24 @@
+from django import template
+
+
+register = template.Library()
+
+
+# Matching labels with what React producing
+# see ./core/frontend/src/js/react/commodities/CodeInput.js
+CODE_BOX_LABELS = {
+    0: "First",
+    1: "Second",
+    2: "Third",
+    3: "Fourth",
+    4: "Fifth",
+}
+
+
+@register.filter
+def code_box_label(counter):
+    """ Used to help generate the label text for commodity input boxes """
+    try:
+        return CODE_BOX_LABELS[counter]
+    except KeyError:
+        return ""
