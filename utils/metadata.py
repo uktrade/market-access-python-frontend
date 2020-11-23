ebf8d278663cb5dd42eac7765147953c55071c49
Feri Csepregi <feri@31.digital>
2020-11-23 15:43:07 +0000

    fixup! MAR-696 - add helper class to manage lists in session variables between views called SessionList - create views to manage government organisations for a barrier - add government organisation filter to public barrier list view

--- utils/metadata.py
+++ utils/metadata.py
@@ -386,6 +386,11 @@ class Metadata:
             for org in self.get_gov_organisations()
         )
 
+    def get_government_organisation(self, org_id):
+        for org in self.get_gov_organisations():
+            if str(org["id"]) == str(org_id):
+                return org
+
     def get_gov_organisations_by_ids(self, list_of_ids):
         list_of_ids = [str(id) for id in list_of_ids]
         return (
