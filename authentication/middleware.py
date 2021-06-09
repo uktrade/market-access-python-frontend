5001012a3330d40ffa7b68221454f6b6dc236ccf
Ciaran Doherty <cad106uk@gmail.com>
2021-06-09 12:00:43 +0100

    debugging

--- authentication/middleware.py
+++ authentication/middleware.py
@@ -23,9 +23,9 @@ class SSOMiddleware:
         self.get_response = get_response
 
     def __call__(self, request):
-        logging.warning(f"STUB:1 {request.user}")
-        client = MarketAccessAPIClient(request.session.get("sso_token"))
-        request.user = client.users.get_current()
+        logging.warning(f"STUB:1 {request.user} {request.session.get('sso_token')}")
+        # client = MarketAccessAPIClient(request.session.get("sso_token"))
+        # request.user = client.users.get_current()
         logging.warning(f"STUB:2 {request.user}")
         tmp = self.get_response(request)
         logging.warning(f"STUB:3 {request.user}")
