efe8cb95c651c42b100e8946ce616f8eb8d91e5c
James Pacileo <jamespacileo@users.noreply.github.com>
2021-12-20 12:30:16 +0000

    MAR-1118 - Added urls to completion items and adusted percetage value

--- barriers/templatetags/completeness.py
+++ barriers/templatetags/completeness.py
@@ -7,13 +7,15 @@ register = template.Library()
 def completeness_total(barrier):
     total = 0
     if barrier.location:
-        total += 10
+        total += 18
     if barrier.summary:
-        total += 10
+        total += 18
     if barrier.source:
-        total += 10
+        total += 16
+    if barrier.sectors:
+        total += 16
     if barrier.categories:
-        total += 10
+        total += 16
     if barrier.commodities:
-        total += 10
+        total += 16
     return total
