e89c6e578c3f3c881b9b5f86202b1dcc7f5f3c5b
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-03-30 16:15:43 +0100

    Shut Black up.

--- barriers/templatetags/assessments.py
+++ barriers/templatetags/assessments.py
@@ -9,7 +9,7 @@ def calculation_to_percentage(value: str, decimal_places: int = 10):
     We assume value of 1 is 100%
     """
     normalized_value = float(value) * 100
-    if normalized_value < 1 / (10**decimal_places):
+    if normalized_value < 1 / (10 ** decimal_places):
         return "0%"
 
     return f"{normalized_value:5f}%"
