4d2625701434655a4847f6c6bf4c437114d2c0cd
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-05-06 10:45:06 +0100

    Filter users by group.

--- core/frontend/src/js/pages/users/select.js
+++ core/frontend/src/js/pages/users/select.js
@@ -1,9 +1,13 @@
 ma.pages.users = {
-    select: function() {
+    enableGroupFilter: function() {
 
         function bindOnSelectionChanged(event) {
             document.querySelector("#filter_group").addEventListener("change", function(event) {
-                debugger;
+                if (this.options[this.selectedIndex].value === 'all') {
+                    self.location.href = `${self.location.origin}${self.location.pathname}`;
+                } else {
+                    this.form.submit();
+                }
             })
         }
 
