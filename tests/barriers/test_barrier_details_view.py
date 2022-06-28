95eaa7ff39a23cd6d27087e4b1b404e20d1958dd
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-06-28 15:48:08 +0100

    Tests for not showing barrier sectors as outstanding if they've been specified.

--- tests/barriers/test_barrier_details_view.py
+++ tests/barriers/test_barrier_details_view.py
@@ -52,3 +52,57 @@ class BarrierViewTestCase(MarketAccessTestCase):
         assert (
             expected_css_class_count == unseen_events_count
         ), f"Expected {expected_css_class_count} unseen events, got: {unseen_events_count}"
+
+
+class BarrierViewOutstandingSectorsTestCase(MarketAccessTestCase):
+    def test_barrier_view_shows_sectors_outstanding_if_no_sectors_specified(self):
+        self.barrier["all_sectors"] = False
+        self.barrier["sectors"] = []
+        sectors_affected_url = reverse(
+            "barriers:edit_sectors", kwargs={"barrier_id": self.barrier["id"]}
+        )
+        sectors_affected_link = f'<a href="{sectors_affected_url}">Sectors affected</a>'
+
+        response = self.client.get(
+            reverse(
+                "barriers:barrier_detail", kwargs={"barrier_id": self.barrier["id"]}
+            )
+        )
+        html = response.content.decode("utf8")
+
+        assert sectors_affected_link in html
+
+    def test_barrier_view_does_not_show_sectors_outstanding_if_a_sector_specified(self):
+        # The default barrier has a sector. Let's hope that doesn't change.
+        sectors_affected_url = reverse(
+            "barriers:edit_sectors", kwargs={"barrier_id": self.barrier["id"]}
+        )
+        sectors_affected_link = f'<a href="{sectors_affected_url}">Sectors affected</a>'
+
+        response = self.client.get(
+            reverse(
+                "barriers:barrier_detail", kwargs={"barrier_id": self.barrier["id"]}
+            )
+        )
+        html = response.content.decode("utf8")
+
+        assert sectors_affected_link not in html
+
+    def test_barrier_view_does_not_show_sectors_outstanding_if_all_sectors_specified(
+        self,
+    ):
+        self.barrier["all_sectors"] = True
+        self.barrier["sectors"] = []
+        sectors_affected_url = reverse(
+            "barriers:edit_sectors", kwargs={"barrier_id": self.barrier["id"]}
+        )
+        sectors_affected_link = f'<a href="{sectors_affected_url}">Sectors affected</a>'
+
+        response = self.client.get(
+            reverse(
+                "barriers:barrier_detail", kwargs={"barrier_id": self.barrier["id"]}
+            )
+        )
+        html = response.content.decode("utf8")
+
+        assert sectors_affected_link not in html
