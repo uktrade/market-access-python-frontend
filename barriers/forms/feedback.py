08ec28676f33d00a3328fa517202b6e8fbb864c9
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-08-25 16:58:21 +0100

    Feedback form and template.

---
+++ barriers/forms/feedback.py
@@ -0,0 +1,34 @@
+from django import forms
+
+
+class FeedbackForm(forms.Form):
+    satisfaction = forms.ChoiceField(
+        label="1. Overall, how do you feel about your use of the Digital Market Access Service (DMAS) today?",
+        choices=(
+            ("VERY_SATISFIED", "Very satisfied"),
+            ("SATISFIED", "Satisfied"),
+            ("NEITHER", "Neither satisfied nor dissatisfied"),
+            ("DISSATISFIED", "Dissatisfied"),
+            ("VERY_DISSATISFIED", "Very dissatisfied"),
+        ),
+        widget=forms.RadioSelect(attrs={"class": "govuk-radios__input"}),
+    )
+    attempted_actions = forms.MultipleChoiceField(
+        label="2. What were you trying to do today?",
+        help_text="Select all that apply.",
+        choices=(
+            ("REPORT_BARRIER", "Report a barrier"),
+            ("PROGRESS_UPDATE", "Set a progress update"),
+            ("EXPORT_BARRIER_CSV", "Export a barrier CSV report"),
+            ("ACTION_PLAN", "Create or edit an action plan"),
+            ("OTHER", "Other"),
+            ("DONT_KNOW", "Don't know"),
+        ),
+        widget=forms.CheckboxSelectMultiple(attrs={"class": "govuk-checkboxes__input"}),
+    )
+    feedback_text = forms.CharField(
+        label="3. How could we improve the service?",
+        help_text="Don't include any personal information, like your name or email address.",
+        max_length=3000,
+        widget=forms.Textarea(attrs={"class": "govuk-textarea", "rows": 7}),
+    )
