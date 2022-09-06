7e8365a6a0582ed6b4b50546d338ffe8b6fdc8bc
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-09-06 12:59:14 +0100

    Allow user to return to where they came from after feedback submitted.

--- barriers/views/feedback.py
+++ barriers/views/feedback.py
@@ -1,3 +1,5 @@
+from urllib.parse import quote
+
 from django.urls import reverse_lazy
 from django.views.generic import FormView, TemplateView
 
@@ -9,10 +11,18 @@ class FeedbackFormView(FormView):
     form_class = FeedbackForm
     success_url = reverse_lazy("core:feedback-thanks")
 
+    def get_context_data(self, **kwargs):
+        kwargs["return_url"] = self.request.GET.get("return", "/")
+        return super().get_context_data(**kwargs)
+
     def get_form_kwargs(self):
         form_kwargs = super().get_form_kwargs()
         form_kwargs["token"] = self.request.session.get("sso_token")
         return form_kwargs
+
+    def get_success_url(self):
+        return_url = quote(self.request.GET.get("return", "/"))
+        return f"{self.success_url}?return={return_url}"
 
     def form_valid(self, form):
         form.save()
@@ -21,3 +31,7 @@ class FeedbackFormView(FormView):
 
 class FeedbackGratitudeView(TemplateView):
     template_name = "feedback-thanks.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs["return_url"] = self.request.GET.get("return", "/")
+        return super().get_context_data(**kwargs)
