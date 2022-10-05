0e528d17b6507dc6c82ad2fa48db0ad29ce0c2f5
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-10-05 16:19:56 +0100

    Add an example test showing use of `reverse` to construct test URL.

--- ui_tests_playwright/test_example.py
+++ ui_tests_playwright/test_example.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from playwright.sync_api import Page, expect
 
 from ui_tests_playwright import settings
@@ -6,3 +7,9 @@ from ui_tests_playwright import settings
 def test_example(page: Page):
     page.goto(settings.BASE_URL)
     expect(page).to_have_title("Market Access - Homepage")
+
+
+def test_example_2(page: Page):
+    search_page_path = reverse("barriers:search")
+    page.goto(f"{settings.BASE_URL}{search_page_path}")
+    expect(page).to_have_title("Market Access - Search")
