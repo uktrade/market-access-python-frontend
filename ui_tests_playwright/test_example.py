1dd4f019b7c6063cdfca05f8730a1b3640a1f144
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-10-05 16:19:05 +0100

    Remove live_server fixture from args as it's now a session-wide autouse fixture.

--- ui_tests_playwright/test_example.py
+++ ui_tests_playwright/test_example.py
@@ -3,6 +3,6 @@ from playwright.sync_api import Page, expect
 from ui_tests_playwright import settings
 
 
-def test_example(live_server, page: Page):
+def test_example(page: Page):
     page.goto(settings.BASE_URL)
     expect(page).to_have_title("Market Access - Homepage")
