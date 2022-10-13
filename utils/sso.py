c027559b2d8e6ed4c767aba955824c0fb403d159
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-10-13 13:53:41 +0100

    Add method to look up Staff SSO user by email address.

--- utils/sso.py
+++ utils/sso.py
@@ -44,3 +44,9 @@ class SSOClient:
         response = self.get(path)
         users = response.get("results", [])
         return users
+
+    def get_user_by_email(selfself, email_address):
+        path = f"user/introspect/?email={quote_plus(email_address)}"
+        response = self.get(path)
+        users = response.get("results", [])
+        return users
