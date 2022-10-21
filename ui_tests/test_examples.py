25b483eff1a5105495a91c9edc912c4e472ca8e8
Nick Fitzsimons <nick@nickfitz.co.uk>
2022-10-21 17:00:02 +0100

    UI tests with playwright (#350)
    * Use the same version of Lunux as is used in production.
    
    * Different system requirements and package names! Also, node is an idiot.
    
    * There doesn't seem to be a Python 3.7 available from the official Ubuntu repository, but the excellent deadsnakes repo comes to the rescue!
    
    * Switch wget for curl, as we already used that further up.
    
    * No need to upgrade pip - further up, we installed the latest version.
    
    * Bump node to a more recent version.
    
    * Update node packages.
    
    * added rtp tag to metadata (#348)
    
    * Add Playwright to project requirements.
    
    * Start setting up Playwright tests.
    
    * Install and initialise Playwright in Docker.
    
    * Configure Playwright to use test settings.
    
    * Custom Playwright settings.
    
    * Configure Django test server and Mock SSO.
    
    * An example of a Playwright test 🙂
    
    * Ensure the live_server fixture is in use for all tests.
    
    * Remove live_server fixture from args as it's now a session-wide autouse fixture.
    
    * Add an example test showing use of `reverse` to construct test URL.
    
    * More examples and a test for history.
    
    * Hotfix for the pre-commit problem with outdated flake8.
    
    * TSS-477: BUGFIX barrier status history (#354)
    
    * This needs redoing.
    
    * Show all status changes for PB100 barriers.
    
    * Update view tests to reflect new, more inclusive attitude to history.
    
    * Missing </div>.
    
    * Need the lockfile too…
    
    * Update CircleCI images.
    
    * Well, that just broke everything :-/
    
    * Let's try just switching images for the formats-and-secrets checks, as they're what's failing.
    
    * According to https://github.com/CircleCI-Public/cimg-python/issues/112#issuecomment-1248784529 the problem is using `sudo pip install` - removing the `sudo` suppsedly fixes it. Let's see…
    
    * Aaagh! "Fixed" the wrong bit :-(
    
    * Try force-installing a version of `importlib-metadata` that ought to work.
    
    * Well, that didn't work. Let's dig a little deeper.
    
    * Let's try updating flake8 used by pre-commit.
    
    * Check if we actually need to pin the importlib-metadata version now flake8 is working.
    
    * That just broke stuff again :-(
    
    * Back out the CircleCI changes - they should be done, but we should do them on a separate tech debt ticket :-)
    
    * This is poetry.lock as generated on this branch after rebasing on master.
    
    * End-of-file-fixer being annoying again :-/
    
    * Now flake8 is breaking on a different step, because we install different versions for pre-commit and the main environment. Ho hum.
    
    * Try to bust CircleCI's cache for the pep8 step.
    
    * Restore CircleCI's cache for the pep8 step.
    
    * Mention the need for the API to be running.
    
    * Add comment.
    
    * Get rid of old Selenium tests and move Playwright tests in.
    
    * Run UI tests via Makefile.
    
    * Update README for UI tests.
    
    * Dependency updates after merging master back in.
    
    * Include TEST_BASE_URL in sample Docker env file.
    
    * Mention problem with local Barrier IDs.
    
    Co-authored-by: James Pacileo <608868+jamespacileo@users.noreply.github.com>

---
+++ ui_tests/test_examples.py
@@ -0,0 +1,45 @@
+from unittest.mock import Mock, patch
+
+from django.urls import reverse
+from playwright.sync_api import Page, expect
+
+from ui_tests import settings
+from ui_tests.settings import BASE_URL
+
+
+def test_example(page: Page):
+    """
+    Most basic example: load the root URL of the site and check the title
+    N.B. This test will fail if the API isn't running
+    """
+    page.goto(settings.BASE_URL)
+    expect(page).to_have_title("Market Access - Homepage")
+
+
+def test_example_2(page: Page):
+    """
+    Use reverse() to get the URL path component for pages,
+    but you still need BASE_URL
+    N.B. This test will fail if the API isn't running
+    """
+    search_page_path = reverse("barriers:search")
+    page.goto(f"{settings.BASE_URL}{search_page_path}")
+    expect(page).to_have_title("Market Access - Search")
+
+
+@patch("utils.api.resources.BarriersResource.get_full_history")
+def test_example_patch_api(mock_history: Mock, page: Page, barrier_history):
+    """
+    This example shows how to patch an API call to return a value from a fixture
+    Fixtures are defined in conftest.py
+    N.B. Although this successfully mocks the patched method, other API calls
+         still happen, such as to /whoami.
+    """
+    mock_history.return_value = barrier_history
+    history_path = reverse(
+        "barriers:history",
+        kwargs={"barrier_id": "7daba55b-f421-4952-98ee-4f5b408f0af3"},
+    )
+    page.goto(f"{BASE_URL}{history_path}")
+    history_items = page.locator(".history-item")
+    expect(history_items).to_have_count(9)
