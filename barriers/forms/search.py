3677c5fc9e725b0f02b958f4a4c2527db5e2c743
Santino Molinaro <85284133+Santino-Trade@users.noreply.github.com>
2023-01-25 10:38:47 +0000

    Adding helptext to top priority search checkbox (#395)
    Co-authored-by: santinomolinaro <santino.molinaro@mail>

--- barriers/forms/search.py
+++ barriers/forms/search.py
@@ -61,6 +61,8 @@ class BarrierSearchForm(forms.Form):
             ("REMOVAL_PENDING", "Pending removal"),
             ("RESOLVED", "Resolved top 100 priority barrier"),
         ),
+        # Provide tuple and match to choices to display help text relevent to choice
+        help_text=(("APPROVED", "Includes removal pending"),),
         required=False,
     )
     priority_level = forms.MultipleChoiceField(
