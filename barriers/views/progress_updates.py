ec86e34250c4ff889c437329bead0cb3bf6046b2
ferozerub <feroze@taridigital.com>
2023-02-21 18:03:21 +0000

    Refactor - rename js file, update default values

--- barriers/views/progress_updates.py
+++ barriers/views/progress_updates.py
@@ -47,8 +47,13 @@ class BarrierAddTop100ProgressUpdate(APIBarrierFormViewMixin, FormView):
             proposed_date = self.barrier.proposed_estimated_resolution_date
         else:
             proposed_date = self.barrier.estimated_resolution_date
+        if self.barrier.estimated_resolution_date_change_reason:
+            proposed_reason = self.barrier.estimated_resolution_date_change_reason
+        else:
+            proposed_reason = None
         return {
-            "estimated_resolution_date": self.barrier.proposed_estimated_resolution_date
+            "estimated_resolution_date": proposed_date,
+            "estimated_resolution_date_change_reason": proposed_reason,
         }
 
     def get_form_kwargs(self):
@@ -141,10 +146,20 @@ class BarrierEditProgressUpdate(APIBarrierFormViewMixin, FormView):
         )
         updates = self.barrier.progress_updates
         progress_update_id = self.kwargs.get("progress_update_id")
+        if self.barrier.proposed_estimated_resolution_date:
+            proposed_date = self.barrier.proposed_estimated_resolution_date
+        else:
+            proposed_date = self.barrier.estimated_resolution_date
+        if self.barrier.estimated_resolution_date_change_reason:
+            proposed_reason = self.barrier.estimated_resolution_date_change_reason
+        else:
+            proposed_reason = None
         return {
             "status": progress_update["status"],
             "update": progress_update["message"],
             "next_steps": progress_update["next_steps"],
+            "estimated_resolution_date": proposed_date,
+            "estimated_resolution_date_change_reason": proposed_reason,
         }
 
 
