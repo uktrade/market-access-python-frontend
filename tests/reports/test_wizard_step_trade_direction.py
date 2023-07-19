ce460d400ac9214898bf8b741385b08b40274c54
santinomolinaro <santino.molinaro@mail>
2023-07-19 14:54:17 +0100

    Trade direction step unit tests

--- tests/reports/test_wizard_step_trade_direction.py
+++ tests/reports/test_wizard_step_trade_direction.py
@@ -1,6 +1,49 @@
-# CASES TO TEST #
-#
-# 1. Happy path - Continue with data passes onto sectors step -
-#    ensure session data saved to DB & cleaned_data correct
-#
-# 2. Error - Continue without entering value for trade_direction
+import logging
+
+from core.tests import MarketAccessTestCase
+from reports.report_barrier_forms import BarrierTradeDirectionForm
+
+logger = logging.getLogger(__name__)
+
+
+class ReportWizardTradeDirectionStepTestCase(MarketAccessTestCase):
+    # make django-test path=reports/test_wizard_step_trade_direction.py::ReportWizardTradeDirectionStepTestCase
+
+    def test_valid_form_entry(self):
+        test_trade_direction = "1"
+        form = BarrierTradeDirectionForm(
+            {
+                "trade_direction": test_trade_direction,
+            }
+        )
+
+        # Assert is_valid first before running clean so cleaned_data is set
+        assert form.is_valid()
+        form.clean()
+        assert test_trade_direction in form.cleaned_data["trade_direction"]
+
+    def test_invalid_form_entry(self):
+        test_trade_direction = "4"
+        form = BarrierTradeDirectionForm(
+            {
+                "trade_direction": test_trade_direction,
+            }
+        )
+
+        # Assert is_valid first before running clean so cleaned_data is set
+        assert form.is_valid() is False
+        error_message = "Select a valid choice"
+        assert error_message in str(form.errors["trade_direction"])
+
+    def test_missing_form_entry(self):
+        test_trade_direction = None
+        form = BarrierTradeDirectionForm(
+            {
+                "trade_direction": test_trade_direction,
+            }
+        )
+
+        # Assert is_valid first before running clean so cleaned_data is set
+        assert form.is_valid() is False
+        error_message = "Select the trade direction this barrier affects"
+        assert error_message in form.errors["trade_direction"]
