c94190a886eab7fc9b8e8a8a63fb818c8bf11f63
Uka Osim <hsojo91@gmail.com>
2023-09-12 10:42:14 +0100

    fixing broken tests

--- barriers/forms/search.py
+++ barriers/forms/search.py
@@ -569,7 +569,7 @@ class BarrierSearchForm(forms.Form):
         params["export_types"] = ",".join(self.cleaned_data.get("export_types", []))
         params["start_date"] = self.format_start_date()
         params["only_main_sector"] = (
-            "yes" if self.cleaned_data.get("only_main_sector") else "no"
+            "yes" if self.cleaned_data.get("only_main_sector") else None
         )
 
         return {k: v for k, v in params.items() if v}
