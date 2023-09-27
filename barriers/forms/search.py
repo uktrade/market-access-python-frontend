c56d2fb390c98772107aee043db9279e402d0e18
christopherpettinga <chrispettinga@gmail.com>
2023-09-27 11:39:05 +0100

    tag choices are now ordered according to metadata.

--- barriers/forms/search.py
+++ barriers/forms/search.py
@@ -426,7 +426,6 @@ class BarrierSearchForm(forms.Form):
             for tag in self.metadata.get_barrier_tag_choices("search")
             if tag["title"] not in DEPRECATED_TAGS
         ]
-        choices.sort(key=itemgetter(0))
         self.fields["tags"].choices = choices
 
     def set_ordering_choices(self):
