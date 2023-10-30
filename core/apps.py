ef1f4143c356ff3c6f96d9cb438a3ac0fe2a1f57
christopherpettinga <chrispettinga@gmail.com>
2023-10-30 16:21:45 +0000

    deleeting metadata on deployment

--- core/apps.py
+++ core/apps.py
@@ -1,5 +1,12 @@
+import redis
 from django.apps import AppConfig
+from django.conf import settings
 
 
 class CoreConfig(AppConfig):
     name = "core"
+
+    def ready(self):
+        if settings.DJANGO_ENV != "test":
+            redis_client = redis.Redis.from_url(url=settings.REDIS_URI)
+            redis_client.delete("metadata")
