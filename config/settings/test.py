882115a236f075021ba777008a735d1d8bdb4b5b
Christopher Pettinga <chrispettinga@gmail.com>
2023-11-01 15:27:12 +0000

    Feature - TSS-1052 - TSS-1051 - TSS-891 - Frontend testing foundation (#538)
    * updated dockerfile to build on mac sonoma
    
    * fixing playwright tests
    
    * Added new test_frontend package that uses Playwright to run integration front-end tests against DMAS, requires a live operating Python frontend and API to be running in the background.
    
    Base class which creates a new barrier for testing purposes.
    
    Added tests for adding barrier tags, and changing barrier statuses
    
    * linting
    
    * removed need for separate config file for front-end tests.
    
    * removed front_end_tests.py
    
    * docker file update for ui
    
    * removed playwright-pytest and just using actual playwright instead.
    
    Added env vars to docker-compose.local-template.env
    
    * using pytest.ini file as I suspect this issue is to do with authorisation
    
    * Got front-end tests working in docker. Updated README.md to document the changes, same for the docker-compose.local-template.env.
    
    * correcting readme numbering
    
    * removed unnecessary middleware removal
    
    * updating docker-compose.local-template.env with mocksso instead of localhost
    
    * Updated test config
    
    * deletign front_end_tests.py as all variables are now in test.py
    
    * Test barriers created are now archived at the end of each test class run.
    
    PAge object is dynamically opened and closed for each test
    
    * Basic create Progress update integration test
    
    * adding pytest-order to poetry lock file
    
    * testing async ruin
    
    * Update test name
    
    * merging with master and regenerating poetry.lock
    
    * adding coverage back to pyproject.toml
    
    ---------
    
    Co-authored-by: Uka Osim <hsojo91@gmail.com>
    Co-authored-by: Uka Osim <ukaosim@dit000130.home>
    Co-authored-by: Feroze Rub <feroze@taridigital.com>

--- config/settings/test.py
+++ config/settings/test.py
@@ -20,6 +20,11 @@ CACHES = {
     }
 }
 
-# Overrides to be able to run individual tests from PyCharm
-# TEMPLATES[0]["DIRS"].append("/usr/src/app/templates")
-# STATIC_ROOT = "/usr/src/app/static"
+
+HEADLESS = env.bool("HEADLESS", default=True)
+
+BASE_FRONTEND_TESTING_URL = env.str(
+    "BASE_FRONTEND_TESTING_URL", default="http://web:9000"
+)
+if BASE_FRONTEND_TESTING_URL.endswith("/"):
+    BASE_FRONTEND_TESTING_URL = BASE_FRONTEND_TESTING_URL[:-1]
