57fff001c36b8bacaa3f41e6bb09f49ca77fdeeb
christopherpettinga <chrispettinga@gmail.com>
2023-11-02 16:40:41 +0000

    updating CSP policy

--- config/settings/hardening.py
+++ config/settings/hardening.py
@@ -19,6 +19,8 @@ CSRF_COOKIE_SECURE = True
 CSRF_COOKIE_HTTPONLY = True
 
 CSP_DEFAULT_SRC = ("'self'",)
+CSP_SCRIPT_SRC = ("'self'", "https://www.googletagmanager.com/", "'unsafe-inline'")
+
 CSP_REPORT_URI = env.str("CSP_REPORT_URI", default=None)
 CSP_REPORT_ONLY = env.bool("CSP_REPORT_ONLY", default=True)
 
