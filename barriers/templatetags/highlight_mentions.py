677124057cfa8695aea8d360110e38d08dbf60b6
Asmund Bekker <asmundbekker@DBT000435.local>
2023-11-07 12:33:55 +0000

    f

--- barriers/templatetags/highlight_mentions.py
+++ barriers/templatetags/highlight_mentions.py
@@ -30,7 +30,7 @@ def get_mention_emails(value, user_email=None, autoescape=True):
     regex = r"(@[^ @]+@[^ @\r\n]+)"
 
     def wrap_email(match):
-        email = match.group(1)
+        email = match.group(1)[1:]  # Remove leading @ for a clean email value
         return f'{email},'
 
     result = re.sub(regex, wrap_email, value, 0, re.MULTILINE)
