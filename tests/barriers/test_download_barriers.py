5b3068c5caff18833ff5ff388f9f8492b3686e7f
Uka Osim <uka.osim@digital.trade.gov.uk>
2024-02-13 15:54:30 +0000

    fixing broken tests

--- tests/barriers/test_download_barriers.py
+++ tests/barriers/test_download_barriers.py
@@ -3,8 +3,8 @@ from http import HTTPStatus
 from django.urls import reverse
 from mock import patch
 
-from core.tests import MarketAccessTestCase
 from barriers.models import BarrierDownload
+from core.tests import MarketAccessTestCase
 
 
 class TestDownloadBarriers(MarketAccessTestCase):
@@ -15,8 +15,9 @@ class TestDownloadBarriers(MarketAccessTestCase):
         }
         download_id = "83756b9a-5d95-e211-a939-e4115bead28a"
         response = self.client.get(
-            reverse("barriers:download-link", kwargs={"download_barrier_id": download_id}),
-
+            reverse(
+                "barriers:download-link", kwargs={"download_barrier_id": download_id}
+            ),
         )
         assert response.status_code == HTTPStatus.FOUND
 
@@ -40,7 +41,6 @@ class TestDownloadBarriers(MarketAccessTestCase):
                 "ordering": "-reported",
             },
             "count": 10,
-
         }
         mock_create.return_value = BarrierDownload(data)
         response = self.client.get(
@@ -61,13 +61,16 @@ class TestDownloadBarriers(MarketAccessTestCase):
         assert "&country=83756b9a-5d95-e211-a939-e4115bead28a" in response.url
 
         mock_create.assert_called_with(
-            **{'search': 'Test search', 'location': '9f5f66a0-5d95-e211-a939-e4115bead28a,83756b9a-5d95-e211-a939-e4115bead28a', 'archived': '0', 'ordering': '-reported'}
+            **{
+                "search": "Test search",
+                "location": "9f5f66a0-5d95-e211-a939-e4115bead28a,83756b9a-5d95-e211-a939-e4115bead28a",
+                "archived": "0",
+                "ordering": "-reported",
+            }
         )
 
     @patch("utils.api.client.BarrierDownloadsResource.create")
-    def test_download_response_contains_correct_url_encoding(
-        self, mock_create
-    ):
+    def test_download_response_contains_correct_url_encoding(self, mock_create):
         # tss-1359 - filters missing after download redirect
 
         data = {
@@ -86,10 +89,9 @@ class TestDownloadBarriers(MarketAccessTestCase):
                 "ordering": "-reported",
             },
             "count": 10,
-
         }
         mock_create.return_value = BarrierDownload(data)
- 
+
         response = self.client.get(
             reverse("barriers:download"),
             data={
