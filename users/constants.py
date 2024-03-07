0c37d5558dc0975d9fbdf90dd2ad1047ecada53e
abarolo <asmundbekker@DBT000435.local>
2024-03-07 16:13:42 +0000

    format

--- users/constants.py
+++ users/constants.py
@@ -4,7 +4,7 @@ USER_ADDITIONAL_PERMISSION_GROUPS = [
     "Download approved user",
     "Action plan user",
     "PB100 barrier approver",
-    "Related Barriers"
+    "Related Barriers",
 ]
 
 REGIONAL_LEAD_PERMISSION_GROUPS = [
