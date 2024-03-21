6233b22659d3cde1cbc9289c0d98a1fe364d8163
Uka Osim <uka.osim@digital.trade.gov.uk>
2024-03-21 15:12:53 +0000

    changing to headless

--- test_frontend/conftest.py
+++ test_frontend/conftest.py
@@ -18,7 +18,7 @@ def playwright_instance():
 
 @pytest.fixture(scope="session")
 def browser(playwright_instance):
-    browser = playwright_instance.chromium.launch(headless=False)
+    browser = playwright_instance.chromium.launch(headless=HEADLESS)
     yield browser
     browser.close()
 
