232e771eb1fa82b1235641e7ce1cf47d447f675c
abarolo <asmundbekker@DBT000435.local>
2024-03-25 09:46:43 +0000

    TSS-1614: Xray

--- config/wsgi.py
+++ config/wsgi.py
@@ -6,7 +6,8 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.0/howto/deployment/wsgi/
 """
-
 from django.core.wsgi import get_wsgi_application
+from opentelemetry.instrumentation.wsgi import OpenTelemetryMiddleware
 
 application = get_wsgi_application()
+application = OpenTelemetryMiddleware(application)
