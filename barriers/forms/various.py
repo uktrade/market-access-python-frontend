f1ab814bcd55692b0a3428241bc7fb97151d2de1
abarolo <asmundbekker@DBT000435.local>
2024-04-25 10:54:39 +0100

    tss-1689: Progress type help text

--- barriers/forms/various.py
+++ barriers/forms/various.py
@@ -1,12 +1,22 @@
 from django import forms
+from django.utils.safestring import mark_safe
 
+
+from utils.forms import fields
 
 class ChooseUpdateTypeForm(forms.Form):
     update_type = forms.ChoiceField(
         choices=(
-            ("top_100_priority", "Barrier progress"),
-            ("programme_fund", "Programme Fund"),
+            ("top_100_priority", mark_safe(
+                "<span class='govuk-body'>Barrier progress</span> <span class='govuk-hint'>Monthly updates for"
+                " PB100 barriers, including any using the Regulator Fund</span>"
+            )),
+            ("programme_fund", mark_safe(
+                "<span class='govuk-body'>Programme Fund</span> <span class='govuk-hint'>Regular updates on barriers"
+                " using the Facilitative Regional Funds</span>"
+            )),
         ),
+        help_text=('a', 'b'),
         error_messages={
             "required": "Select barrier progress or Programme Fund",
         },
