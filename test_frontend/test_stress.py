28992f0adf729ad4042d2c987431925ac0d7d44d
Uka Osim <uka.osim@digital.trade.gov.uk>
2024-05-01 13:48:23 +0100

    stress test

---
+++ test_frontend/test_stress.py
@@ -0,0 +1,21 @@
+import os
+from .utils import get_base_url
+
+
+NUM_CONCURRENT_TESTS = int(os.getenv("NUM_CONCURRENT_TESTS", 20))
+HEADLESS = os.getenv("TEST_HEADLESS", "false").lower() == "true"
+
+
+def test_multiple_users(context):
+    pages = [context.new_page() for _ in range(NUM_CONCURRENT_TESTS)]
+
+    for page in pages:
+        page.goto(f"{get_base_url()}search/")
+        # perform a search
+        page.get_by_role("textbox", name="Search").fill("test")
+        page.get_by_text("Goods", exact=True).click()
+
+    # Clean up after the test
+    for page in pages:
+        page.close()
+    context.close()
