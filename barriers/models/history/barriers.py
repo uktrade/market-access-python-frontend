88ed786dc731b01174a3380c66a94fde10a0b2f1
EPedley <lizzypedley@gmail.com>
2024-08-09 16:25:35 +0100

    Adding history functionality

--- barriers/models/history/barriers.py
+++ barriers/models/history/barriers.py
@@ -177,6 +177,18 @@ class OrganisationHistoryItem(BaseHistoryItem):
         ]
         names = [t["name"] for t in organisations]
         return names
+    
+class PolicyTeamsHistoryItem(BaseHistoryItem):
+    field = "policy_teams"
+    field_name = "Policy teams"
+
+    def get_value(self, value):
+        policy_teams_names = [
+            self.metadata.get_policy_team(policy_team).get("title")
+            for policy_team in value or []
+        ]
+        policy_teams_names.sort()
+        return policy_teams_names
 
 
 class PriorityHistoryItem(BaseHistoryItem):
@@ -329,6 +341,7 @@ class BarrierHistoryItem(PolymorphicBase):
         IsSummarySensitiveHistoryItem,
         LocationHistoryItem,
         OrganisationHistoryItem,
+        PolicyTeamsHistoryItem,
         ProductHistoryItem,
         PriorityHistoryItem,
         PublicEligibilitySummaryHistoryItem,
