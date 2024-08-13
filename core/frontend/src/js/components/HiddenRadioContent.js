04dfaaee3fecb7e2c27bf4d5bde1df131a96cda6
EPedley <lizzypedley@gmail.com>
2024-08-13 09:40:08 +0100

    Formatting

--- core/frontend/src/js/components/HiddenRadioContent.js
+++ core/frontend/src/js/components/HiddenRadioContent.js
@@ -1,32 +1,32 @@
 ma.pages.report.hiddenRadioContent = function () {
-
     const showComponent = function (component) {
         component.classList.remove("govuk-radios__conditional--hidden");
         // component.style.visibility = "visible";
     };
     const hideComponent = function (component) {
-            component.classList.add("govuk-radios__conditional--hidden");
-            // component.style.visibility = "hidden";
+        component.classList.add("govuk-radios__conditional--hidden");
+        // component.style.visibility = "hidden";
     };
 
     const updateComponents = function (id) {
         conditionalComponents.forEach(function (currentConditionalComponent) {
             if (currentConditionalComponent.id == "conditional-" + id) {
-                showComponent(currentConditionalComponent)
+                showComponent(currentConditionalComponent);
+            } else {
+                hideComponent(currentConditionalComponent);
             }
-            else {
-                hideComponent(currentConditionalComponent)
-            };
         });
     };
-    
-    const conditionalComponents = document.querySelectorAll(".govuk-radios__conditional");
+
+    const conditionalComponents = document.querySelectorAll(
+        ".govuk-radios__conditional",
+    );
     const radioButtons = document.querySelectorAll(".govuk-radios__input");
 
     radioButtons.forEach(function (currentRadioButton) {
-        showComponent(currentRadioButton)
-        currentRadioButton.addEventListener("click", function() {
-            updateComponents(this.id)
+        showComponent(currentRadioButton);
+        currentRadioButton.addEventListener("click", function () {
+            updateComponents(this.id);
         });
     });
 };
