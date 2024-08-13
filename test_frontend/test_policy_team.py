cfd019d2c875b624355f37d0c79b58baaa206b36
EPedley <lizzypedley@gmail.com>
2024-08-13 15:27:24 +0100

    fixing faulty test

--- test_frontend/test_policy_team.py
+++ test_frontend/test_policy_team.py
@@ -18,16 +18,11 @@ def test_add_policy_team(page, create_test_barrier):
     expect(page.locator(".policy-teams_text")).to_have_text("Customs")
 
 
-def update_add_policy_team(page, create_test_barrier):
+def test_update_policy_team(page, create_test_barrier):
     title = "test"
     url = create_test_barrier(title=title)
     page.goto(clean_full_url(url))
 
-    page.get_by_role("link", name="Add a policy team").click()
-    page.get_by_label("Customs").check()
-    page.get_by_role("button", name="Add").click()
-    page.get_by_role("button", name="Save and return").click()
-    page.get_by_text("Customs").click()
     page.get_by_label("edit policy teams").click()
     page.get_by_role("link", name="Add another policy team").click()
     page.get_by_label("Gender").check()
