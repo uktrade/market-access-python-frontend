18386c867e414637ef7aa23160623dea5ae8b3e2
Uka Osim <uka.osim@digital.trade.gov.uk>
2024-09-10 15:02:11 +0100

    rendering all charts appropriately

--- core/frontend/src/js/react/dashboard/charts.js
+++ core/frontend/src/js/react/dashboard/charts.js
@@ -1,21 +1,59 @@
 import React from "react";
 import Charts from "react-apexcharts";
 
-export const handlePieChart = (/** @type {{ options: ApexCharts.ApexOptions; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData) => {
+export const handlePieChart = (/** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData) => {
+    const options = {
+        ...chartData.options,
+        plotOptions: {
+            pie: {
+                dataLabels: {
+                    enabled: true,
+                    formatter: function (/** @type {string} */ val) {
+                        return `£${val}`;
+                  }, 
+                }
+            }
+        },
+        legend: {
+            show: false
+        }
+    };
+
     return (
         <Charts
-            options={chartData.options}
+            options={options}
             series={chartData.series}
             type={"pie"}
             height={350}
+            labels={chartData.options.labels}
         />
     );
 };
 
-export const handleBarChart = (/** @type {{ options: ApexCharts.ApexOptions; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData) => {
+export const handleBarChart = (/** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData) => {
+    const options = {
+        ...chartData.options,
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: "100%"
+            },
+        },
+        fill: {
+            opacity: 1,
+        },
+        stroke: {
+            width: 50,
+            colors: ['transparent']
+        },
+        dataLabels: {
+            enabled: true
+        },
+        
+    };
     return (
         <Charts
-            options={chartData.options}
+            options={options}
             series={chartData.series}
             type="bar"
             height={350}
@@ -23,14 +61,37 @@ export const handleBarChart = (/** @type {{ options: ApexCharts.ApexOptions; ser
     );
 };
 
-export const handleStackedBarChart = (/** @type {{ options: ApexCharts.ApexOptions; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData) => {
+export const handleStackedBarChart = (/** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData) => {
     const options = {
         ...chartData.options,
+        chart: {
+            ...chartData.options.chart,
+            type: "bar",
+            stacked: true,
+            height: 350,
+            stackType: '100%',
+            toolbar: {
+                show: true
+            },
+        },
         plotOptions: {
             bar: {
                 horizontal: true,
-                columnWidth: '45%',
+                columnWidth: "100%"
             },
+        },
+        fill: {
+            opacity: 1,
+        },
+        stroke: {
+            width: 5,
+            colors: ['transparent']
+        },
+        dataLabels: {
+            enabled: true
+        },
+        legend: {
+            position: 'bottom',
         },
     };
     return (
