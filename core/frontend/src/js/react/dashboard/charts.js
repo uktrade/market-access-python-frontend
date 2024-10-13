ba915ec963b579af5b6f276f571c45378bac1d3c
Uka Osim <hsojo91@gmail.com>
2024-10-13 23:39:19 +0100

    lint fix

--- core/frontend/src/js/react/dashboard/charts.js
+++ core/frontend/src/js/react/dashboard/charts.js
@@ -15,7 +15,7 @@ const chartTheme = {
 };
 
 export const handlePieChart = (
-    /** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData
+    /** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData,
 ) => {
     const options = {
         ...chartData.options,
@@ -23,7 +23,7 @@ export const handlePieChart = (
             enabled: true,
             formatter: function (
                 /** @type {any} */ _val,
-                /** @type {{ w: { globals: { series: { [x: string]: any; }; }; config: { labels: { [x: string]: any; }; }; }; seriesIndex: string | number; }} */ opts
+                /** @type {{ w: { globals: { series: { [x: string]: any; }; }; config: { labels: { [x: string]: any; }; }; }; seriesIndex: string | number; }} */ opts,
             ) {
                 // get the acual value not the percentage
                 const value = opts.w.globals.series[opts.seriesIndex];
@@ -58,7 +58,7 @@ export const handlePieChart = (
 };
 
 export const handleBarChart = (
-    /** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData
+    /** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData,
 ) => {
     const options = {
         ...chartData.options,
@@ -98,7 +98,7 @@ export const handleBarChart = (
 };
 
 export const handleStackedBarChart = (
-    /** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData
+    /** @type {{ options: any; series: ApexAxisChartSeries | ApexNonAxisChartSeries; }} */ chartData,
 ) => {
     const options = {
         ...chartData.options,
