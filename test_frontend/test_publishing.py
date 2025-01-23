981862e9c1d14b702a15d17266aae77333f153ca
EPedley <lizzypedley@gmail.com>
2025-01-23 15:23:44 +0000

    Shuffling tests around

--- test_frontend/test_publishing.py
+++ test_frontend/test_publishing.py
@@ -14,25 +14,47 @@ from test_frontend.utils import (
 LOGGER = logging.getLogger(__name__)
 
 
-def test_successful_publish(page, create_test_barrier):
+def test_publish_general_user(page, create_test_barrier):
     title = "test"
     url = create_test_barrier(title=title)
-    barrier_details_url = clean_full_url(url)
-    if not admin_user(page):
-        pytest.skip("You are not an admin in this environment", allow_module_level=True)
-
-    page.goto(barrier_details_url + "public")
+    change_permissions(
+        page,
+        get_username(page),
+        "General user",
+    )
+    page.goto(clean_full_url(url) + "public")
     page.get_by_role("button", name="Submit for review").click()
-    page.get_by_role("heading", name="This barrier is now awaiting").click()
 
     expect(page.get_by_text("This barrier is now awaiting approval")).to_be_visible()
+    expect(
+        page.get_by_text("This barrier is now awaiting approval")
+    ).not_to_be_visible()
 
+
+def test_publish_general_user(page, create_test_barrier):
+    title = "test"
+    url = create_test_barrier(title=title)
+    change_permissions(
+        page,
+        get_username(page),
+        "General user",
+    )
+    page.goto(clean_full_url(url) + "public")
+    page.get_by_role("button", name="Submit for review").click()
+
+    expect(page.get_by_text("This barrier is now awaiting approval")).to_be_visible()
+    expect(page.get_by_role("link", name="Review barrier")).not_to_be_visible()
+
+
+def test_publish_approver(page, create_test_barrier):
+    title = "test"
+    url = create_test_barrier(title=title)
     change_permissions(
         page,
         get_username(page),
         "Public barrier approver",
     )
-    page.goto(barrier_details_url + "public")
+    page.goto(clean_full_url(url) + "public")
     page.get_by_role("link", name="Review barrier").click()
     page.locator('input[name="content_clearance"]').check()
     page.locator('input[name="external_clearances"]').check()
@@ -43,20 +65,32 @@ def test_successful_publish(page, create_test_barrier):
             "This barrier has been approved and is now with the GOV.UK content team"
         )
     ).to_be_visible()
+    expect(page.get_by_role("link", name="Publish", exact=True)).not_to_be_visible()
 
+    page.get_by_role("button", name="Remove approval").click()
+
+    expect(page.get_by_text("This barrier needs to be approved again")).to_be_visible()
+
+
+def test_publish_publisher(page, create_test_barrier):
+    title = "test"
+    url = create_test_barrier(title=title)
     change_permissions(
         page,
         get_username(page),
         "Publisher",
     )
-    page.goto(barrier_details_url + "public")
+    page.goto(clean_full_url(url) + "public")
+    page.get_by_role("link", name="Review barrier").click()
+    page.locator('input[name="content_clearance"]').check()
+    page.locator('input[name="external_clearances"]').check()
+    page.get_by_role("button", name="Send to GOV.UK content team").click()
     page.get_by_role("link", name="Publish", exact=True).click()
-
     expect(
         page.get_by_text("Confirm you want to publish this barrier on GOV.UK")
     ).to_be_visible()
 
-    # Barriers cannot be published on your local environment
+    # Barriers cannot be published on local environment
     if get_base_url() != "http://market-access.local:9880/":
         page.get_by_role("button", name="Confirm").click()
 
@@ -69,32 +103,12 @@ def test_successful_publish(page, create_test_barrier):
     get_base_url() == "http://market-access.local:9880/",
     reason="Barriers cannot be published on your local environment",
 )
-def test_unpublish(page, create_test_barrier):
+def test_unpublish_publisher(page, create_test_barrier):
     title = "test"
     url = create_test_barrier(title=title)
-    if not admin_user(page):
-        pytest.skip("You are not an admin in this environment", allow_module_level=True)
-
     page.goto(clean_full_url(url) + "public")
     page.get_by_role("link", name="Unpublish").click()
     page.locator("#id_public_publisher_summary").fill("Test")
     page.get_by_role("button", name="Confirm").click()
 
     expect(page.get_by_text("This barrier has been removed")).to_be_visible()
-
-
-def test_remove_review(page, create_test_barrier):
-    title = "test"
-    url = create_test_barrier(title=title)
-    if not admin_user(page):
-        pytest.skip("You are not an admin in this environment", allow_module_level=True)
-
-    change_permissions(
-        page,
-        get_username(page),
-        "Public barrier approver",
-    )
-    page.goto(clean_full_url(url) + "public")
-    page.get_by_role("button", name="Remove approval").click()
-
-    expect(page.get_by_text("This barrier needs to be approved again")).to_be_visible()
