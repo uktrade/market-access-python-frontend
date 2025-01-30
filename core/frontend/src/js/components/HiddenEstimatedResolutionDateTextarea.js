65df786dd463e8f8a54dd58514ea9778238526a5
Feroze Rub <feroze@taridigital.com>
2025-01-30 13:44:06 +0000

    Format

--- core/frontend/src/js/components/HiddenEstimatedResolutionDateTextarea.js
+++ core/frontend/src/js/components/HiddenEstimatedResolutionDateTextarea.js
@@ -1,8 +1,9 @@
 const defaultMonthInputID = "status-date-group-estimated_resolution_date_0";
 const defaultYearInputID = "status-date-group-estimated_resolution_date_1";
 const erdRequiredApprovalWarningID = "erd-requires-approval-warning";
-const erdChangeReason = document.getElementById("estimated_resolution_date_change_reason")
-
+const erdChangeReason = document.getElementById(
+    "estimated_resolution_date_change_reason",
+);
 
 const getMonthValue = (monthInputID) => {
     const monthInput = document.getElementById(monthInputID);
@@ -30,21 +31,19 @@ ma.components.setupHiddenERDTextarea = function (props) {
     let yearInput = document.getElementById(yearInputID);
     const erdWarning = document.getElementById(erdRequiredApprovalWarningID);
     const textarea = document.getElementById(
-        "estimated_resolution_date_change_reason-form-group"
+        "estimated_resolution_date_change_reason-form-group",
     );
     const editErdButton = document.getElementById("edit-erd-link");
     const referenceMonth = getMonthValue(monthInputID);
     const referenceYear = getYearValue(yearInputID);
 
-    console.log("reference date", referenceMonth, referenceYear)
+    console.log("reference date", referenceMonth, referenceYear);
 
-    if (props.is_admin == "True" && (referenceMonth && referenceYear)) {
+    if (props.is_admin == "True" && referenceMonth && referenceYear) {
         // if user is admin and not clearing erd, don't run this code
-        console.log("User is admin exiting")
+        console.log("User is admin exiting");
         return;
     }
-
-
 
     // if user clicks edit button, show textarea
     editErdButton?.addEventListener("click", () => {
@@ -54,13 +53,11 @@ ma.components.setupHiddenERDTextarea = function (props) {
         yearInput.value = proposedDateObject.getFullYear();
     });
 
-    console.log("button watch init")
-
-
+    console.log("button watch init");
 
     // if month and year are empty, exit early
-    if (!(props.is_admin == "True") && (!referenceMonth && !referenceYear)) {
-        console.log("Exiting because reference date is empty")
+    if (!(props.is_admin == "True") && !referenceMonth && !referenceYear) {
+        console.log("Exiting because reference date is empty");
         return;
     }
 
@@ -85,14 +82,15 @@ ma.components.setupHiddenERDTextarea = function (props) {
         if (currentDate > referenceDate) {
             textarea.style.display = "block";
             erdWarning.style.display = "block";
-            console.log("current date not equal to proposed date")
-        }
-        else if (erdChangeReason.value) {
+            console.log("current date not equal to proposed date");
+        } else if (erdChangeReason.value) {
             textarea.style.display = "block";
             erdWarning.style.display = "block";
-            console.log("there is an existing reason so display it", erdChangeReason.value)
-        }
-        else {
+            console.log(
+                "there is an existing reason so display it",
+                erdChangeReason.value,
+            );
+        } else {
             textarea.style.display = "none";
             erdWarning.style.display = "none";
         }
