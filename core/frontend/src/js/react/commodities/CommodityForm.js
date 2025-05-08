011cd0b3cf3f4f3a51e5d31fd35d672c64e1b270
EPedley <lizzypedley@gmail.com>
2025-05-08 15:17:25 +0100

    Formatting

--- core/frontend/src/js/react/commodities/CommodityForm.js
+++ core/frontend/src/js/react/commodities/CommodityForm.js
@@ -10,7 +10,7 @@ function CommodityForm(props) {
     const [codeArray, setCodeArray] = useState(["", "", "", "", ""]);
     const [locationId, setLocationId] = useState(props.locations[0]["id"]);
     const [confirmedCommodities, setConfirmedCommodities] = useState(
-        props.confirmedCommodities
+        props.confirmedCommodities,
     );
     const [unconfirmedCommodities, setUnconfirmedCommodities] = useState([]);
     const [pastedCodes, setPastedCodes] = useState("");
@@ -74,7 +74,7 @@ function CommodityForm(props) {
 
     const getCode = () => {
         let cleanedCodeArray = codeArray.map((element) =>
-            ("00" + element).slice(-2)
+            ("00" + element).slice(-2),
         );
         let zeroPaddedCode = cleanedCodeArray.slice(0, 10).join("");
         return zeroPaddedCode.replace(/0+$/g, "");
@@ -86,12 +86,12 @@ function CommodityForm(props) {
         unconfirmedCommodities.splice(index, 1);
         setUnconfirmedCommodities([...unconfirmedCommodities]);
         clearCodeInput();
-        inputRefContainer.current[0].focus()
+        inputRefContainer.current[0].focus();
     };
 
     const confirmAll = (event) => {
         setConfirmedCommodities(
-            confirmedCommodities.concat(unconfirmedCommodities)
+            confirmedCommodities.concat(unconfirmedCommodities),
         );
         setUnconfirmedCommodities([]);
     };
@@ -99,7 +99,7 @@ function CommodityForm(props) {
     const removeCommodity = (event, index) => {
         confirmedCommodities.splice(index, 1);
         setConfirmedCommodities([...confirmedCommodities]);
-        inputRefContainer.current[0].focus()
+        inputRefContainer.current[0].focus();
     };
 
     async function lookupCode(code) {
@@ -122,7 +122,8 @@ function CommodityForm(props) {
                         let zeroPaddedCode = code.padEnd(10, "0");
                         if (
                             confirmedCommodities.some(
-                                (commodity) => commodity.code === zeroPaddedCode
+                                (commodity) =>
+                                    commodity.code === zeroPaddedCode,
                             )
                         ) {
                             setUnconfirmedCommodities([]);
@@ -139,7 +140,7 @@ function CommodityForm(props) {
                     setIsLoading(false);
                     setUnconfirmedCommodities([]);
                     setCodeLookupError(error);
-                }
+                },
             );
     }
 
@@ -169,7 +170,7 @@ function CommodityForm(props) {
                     if (result["status"] == "ok") {
                         let newCommodities = result.data.filter((item) => {
                             return !confirmedCommodities.some(
-                                (commodity) => commodity.code === item.code
+                                (commodity) => commodity.code === item.code,
                             );
                         });
                         setUnconfirmedCommodities(newCommodities);
@@ -183,7 +184,7 @@ function CommodityForm(props) {
                     setIsLoading(false);
                     setUnconfirmedCommodities([]);
                     setCodeLookupError(error);
-                }
+                },
             );
     }
 
