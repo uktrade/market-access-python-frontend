96631a0b8ddb24a4cd4949a4bd5565c62671cf27
asmund bekker <145478290+abarolo@users.noreply.github.com>
2025-05-19 11:48:29 +0100

    tss-2623: Example month and year in date help text (#935)
    Co-authored-by: abarolo <asmundbekker@DBT000435.local>

--- barriers/forms/edit.py
+++ barriers/forms/edit.py
@@ -753,7 +753,7 @@ class NextStepsItemForm(APIFormMixin, forms.Form):
 class UpdateBarrierStartDateForm(ClearableMixin, APIFormMixin, forms.Form):
     start_date = MonthYearInFutureField(
         label="When did or will the barrier start to affect trade?",
-        help_text="If you don’t know the month, enter 06.",
+        help_text="For example, 3 2007. If you don’t know the month, enter 06.",
         error_messages={"required": "Enter a barrier start date"},
         required=False,
     )
