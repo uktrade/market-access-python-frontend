f93c6fe2c66f84e5a81b8f2f3d9e7005214326c6
EPedley <lizzypedley@gmail.com>
2025-05-29 12:50:43 +0100

    Formatting

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from django.views.generic import TemplateView
 
 from barriers.views.feedback import FeedbackFormView, FeedbackGratitudeView
 from utils.company_search import SearchCompany
